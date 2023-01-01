272bf3652e7d678cced3f590292f05a79cec29c8
Christian Drexler <christian@konsolenknecht.de>
2023-01-01 16:15:18 +0100

    linted, added prefix for part1/part2 to output

--- day1/day1.py
+++ day1/day1.py
@@ -7,12 +7,13 @@ calorieslist = []
 with open(infile) as input:
     calories = 0
     for line in input.readlines():
-        if line != '\n':
+        if line != "\n":
             calories += int(line.strip())
         else:
             calorieslist.append(calories)
             calories = 0
             continue
-print(sorted(calorieslist)[-1])
-print(sum(sorted(calorieslist)[-3:]))
-        
+# part1
+print(f"Result part1: {sorted(calorieslist)[-1]}")
+# part2
+print(f"Result part2: {sum(sorted(calorieslist)[-3:])}")
