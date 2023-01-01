b241cb4982d1884ae367f09703e6b9c408b4f1ce
Christian Drexler <christian@konsolenknecht.de>
2023-01-01 16:29:21 +0100

    formatted code, added readme

--- day2/day2.py
+++ day2/day2.py
@@ -5,82 +5,85 @@
 sum_overall = 0
 
 rps = {
-    'A': {'name': 'rock', 'points': 1, 'loseagainst': 'Y'},
-    'B': {'name': 'paper', 'points': 2, 'loseagainst': 'Z'},
-    'C': {'name': 'scissors', 'points': 3, 'loseagainst': 'X'},
-    'X': {'name': 'rock', 'points': 1},
-    'Y': {'name': 'paper', 'points': 2},
-    'Z': {'name': 'scissors', 'points': 3},
+    "A": {"name": "rock", "points": 1, "loseagainst": "Y"},
+    "B": {"name": "paper", "points": 2, "loseagainst": "Z"},
+    "C": {"name": "scissors", "points": 3, "loseagainst": "X"},
+    "X": {"name": "rock", "points": 1},
+    "Y": {"name": "paper", "points": 2},
+    "Z": {"name": "scissors", "points": 3},
 }
 
 rps2 = {
-    'A': {'name': 'rock', 'points': 1, 'loseagainst': 'B'},
-    'B': {'name': 'paper', 'points': 2, 'loseagainst': 'C'},
-    'C': {'name': 'scissors', 'points': 3, 'loseagainst': 'A'},
-    'X':  0,
-    'Y':  3,
-    'Z':  6
+    "A": {"name": "rock", "points": 1, "loseagainst": "B"},
+    "B": {"name": "paper", "points": 2, "loseagainst": "C"},
+    "C": {"name": "scissors", "points": 3, "loseagainst": "A"},
+    "X": 0,
+    "Y": 3,
+    "Z": 6,
 }
 
 
-sample_input = ( ["A", "Y"], ["B", "X"], ["C", "Z"] )
+sample_input = (["A", "Y"], ["B", "X"], ["C", "Z"])
 
 
 def eval_choices(first, second):
     #
-    if rps[first]['name'] == rps[second]['name']:
+    if rps[first]["name"] == rps[second]["name"]:
         print(f"""{rps[first]['name']} is equal to {rps[second]['name']}""")
-        return 3  + rps[second]['points']
-    elif rps[first]['loseagainst'] == second:
+        return 3 + rps[second]["points"]
+    elif rps[first]["loseagainst"] == second:
         print(f"""{rps[first]['name']} loses to {rps[second]['name']}""")
-        return 6 + rps[second]['points']
+        return 6 + rps[second]["points"]
     else:
         print(f"""{rps[first]['name']} wins against {rps[second]['name']}""")
-        return rps[second]['points']
+        return rps[second]["points"]
+
 
 def find_loser(winner):
-    for k in ['A', 'B', 'C']:
-        if rps2[k]['loseagainst'] == winner:
+    for k in ["A", "B", "C"]:
+        if rps2[k]["loseagainst"] == winner:
             return k
-    
+
+
 def eval2(first, second):
-    if second == 'Y':
-        print(f"""draw, {first}  equal to {first}""")
-        return rps2[first]['points']  + rps2[second]
-    elif second == 'Z':
-        loser = rps2[first]['loseagainst']
-        print(f"""lose, {loser} loses against {first}""")
-        return rps2[loser]['points'] + rps2[second]
-    else: # second is Z
+    if second == "Y":
+        # print(f"""draw, {first}  equal to {first}""")
+        return rps2[first]["points"] + rps2[second]
+    elif second == "Z":
+        loser = rps2[first]["loseagainst"]
+        # print(f"""lose, {loser} loses against {first}""")
+        return rps2[loser]["points"] + rps2[second]
+    else:  # second is Z
         loser = find_loser(first)
-        print(f"""win, {first} wins against {loser}""") 
-        return rps2[loser]['points'] + rps2[second]
+        # print(f"""win, {first} wins against {loser}""")
+        return rps2[loser]["points"] + rps2[second]
+
 
 if __name__ == "__main__":
     for s in sample_input:
         sum_overall += eval_choices(s[0], s[1])
-    print(sum_overall)
+    # print(sum_overall)
     sum_overall = 0
-    # with open("day2-input.txt") as input:
-    #     for line in input.readlines():
-    #         line = line.strip()
-    #         first, second = line.split()
-    #         #print(first, second)
-    #         i = eval_choices(first, second)
-    #         print(i)
-    #         sum_overall += i
-    # print("First part: ", sum_overall)
+    with open("day2-input.txt") as input:
+        for line in input.readlines():
+            line = line.strip()
+            first, second = line.split()
+            # print(first, second)
+            i = eval_choices(first, second)
+            # print(i)
+            sum_overall += i
+    print("Part1: ", sum_overall)
     sum2 = 0
     for s in sample_input:
         sum2 += eval2(s[0], s[1])
-    print("part2: ", sum2)
+    # print("part2: ", sum2)
     sum2 = 0
     with open("day2-input.txt") as input:
         for line in input.readlines():
             line = line.strip()
             first, second = line.split()
             i = eval2(first, second)
-            print(i)
+            # print(i)
             sum2 += i
 
-    print ("Part2: ", sum2)
+    print("Part2: ", sum2)
---
+++ day2/readme.md
@@ -0,0 +1,6 @@
+## Day2
+
+the challenge here was to model the game rules in a data structure. I have coerced the rules into a dictionary, something that works for three parts but would become unpractical with more parts.
+
+the smart way to model the rules would probably have been some sort of ring structure that wraps around at the end (r < p < s < r) but I did not manage to achieve this in python.
+
